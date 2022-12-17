f6eb53d11a8b39be37fdaf705f388d28c139b1c4
Niteshkumar <nkumar35101@gmail.com>
2022-12-17 19:42:52 +0530

    footer added

---
+++ frontend/components/layout/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Footer } from 'flowbite-react';
+
+const Footer_01 = () => {
+  return (
+    <div className="w-full text-center py-8 px-10">
+      <hr></hr>
+      <div className="mt-8 text-slate-300">
+        &copy; 2022 Drift-DAO™
+      </div>
+    </div>
+  );
+}
+
+export default Footer_01
--- frontend/pages/onBoardDAO.js
+++ frontend/pages/onBoardDAO.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer_01 from '../components/layout/Footer';
 import OnBoardDAO from '../components/onBoardDAO/OnBoardDAO';
 
 const onBoardDAO = () => {
 	return (
 		<div className="bg-[url('/images/bg.jpg')]  bg-fixed" >
 			<OnBoardDAO />
+			<Footer_01/>
 		</div>
 	);
 };
